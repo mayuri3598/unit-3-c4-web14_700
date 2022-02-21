e2b59e74fdd79a6574e663461b72c095081171d8
mayuri3598 <97494267+mayuri3598@users.noreply.github.com>
2022-02-21 17:48:07 +0530

    Add files via upload

---
+++ U3C4 - Template/Template-U3C4/components/sidebar.js
@@ -0,0 +1,12 @@
+function navbar(){
+    return `<h1>Masai News</h1>
+    <div>
+     <a href="#"><p>Login</p></a>
+        <input type="search" placeholder="Search News" id="search_box"/>
+        <a href="#"><p>Start Ups</p></a> 
+        <a href="#"><p>News Letters</p></a>
+        <a href="#"><p>Audio</p></a> 
+        <a href="#"><p>Video</p></a> 
+    </div>`
+}
+export default navbar
